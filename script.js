38292292d420fcb93b4b5490c8c0787bdc640885
hunafazaky <hunafa.zaky@gmail.com>
2025-11-13 23:01:55 +0700

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// === GET ELEMENT ===
 const textInput = document.querySelector("#text-input");
 const checkBtn = document.querySelector("#check-btn");
 const result = document.querySelector("#result");
 
-function palindrome(str) {
+// === MAIN FUNCTION ===
+function isPalindrome(str) {
   const arr = str
     .toLowerCase()
     .replace(/[^0-9a-zA-Z]/g, "")
@@ -11,15 +13,16 @@ function palindrome(str) {
   else return false;
 }
 
+// === EVENT FUNCTION ===
 checkBtn.addEventListener("click", () => {
-  console.log(textInput.value);
-  if (textInput.value.length <= 0) {
-    alert("Please input a value.");
-  } else {
-    const final = palindrome(textInput.value);
-    result.innerHTML = `<h1>${textInput.value} is ${
-      final ? "" : "not"
-    } a palindrome</h1>`;
+  if (textInput.value === "") alert("Please input a value.");
+  else {
+    const answer = isPalindrome(textInput.value);
+    result.innerHTML = `
+      ${textInput.value} is 
+      ${answer ? "" : "not"} 
+      a palindrome
+    `;
     textInput.value = "";
   }
 });
